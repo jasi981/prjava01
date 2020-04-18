b44324c6b6c97ee754d3a4418a78d78468c98a2b
jasi981 <jasi981@vidalibarraquer.net>
2020-04-18 17:10:44 +0200

    Versió 0.2 del projecte prjava01

---
+++ prjava01/src/prjava01/Prjava01.java
@@ -0,0 +1,39 @@
+package prjava01;
+
+import java.io.*;
+
+/**
+ *
+ * @author Jasmeen Singh <jasi981@vidalibarraquer.net>
+ */
+public class Prjava01 {
+
+    public static void main(String[] args) throws IOException {
+        File f = new File("fitxer.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("<html>");
+            bw.newLine();
+            bw.write("  <head>");
+            bw.newLine();
+            bw.write("    <title>");
+            bw.newLine();
+            bw.write("      Nova p&agrave;gina  web");
+            bw.newLine();
+            bw.write("    </title>");
+            bw.newLine();
+            bw.write("  </head>");
+            bw.newLine();
+            bw.write("  <body>");
+            bw.newLine();
+            bw.write("    <h1>DAM2 m05uf2pr4</h1>");
+            bw.newLine();
+            bw.write("    Nova p&agrave;gina web");
+            bw.newLine();
+            bw.write("  </body>");
+            bw.newLine();
+            bw.write("</html>");
+            bw.newLine();
+            bw.close();
+        }
+    }
+}
